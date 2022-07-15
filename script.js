e74b22402e0808c3c74b977a65e47f07e37f7a50
jl052 <jasonleung0502@gmail.com>
2022-07-15 18:46:24 +0000

    basic function done

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r = document.getElementById("rate").value;
-    var y = document.getElementById("years").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+",</mark>\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+",</mark><br\>in the year <mark>"+year+"</mark>\<br\>";
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
+
+function checkdata(){
+    var cornumber = document.getElementById("principal");
+    if(cornumber.value==0){
+        alert("Enter a positive number");
+        cornumber.focus();
+        return True;
+    }
+    if(cornumber.value<0){
+        alert("Enter a positive number");
+        cornumber.focus();
+        return True;
+    }
+    return false;
+}
